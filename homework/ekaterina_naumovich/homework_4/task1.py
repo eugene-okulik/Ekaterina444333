2aab04f6d1216af13bbcb39e31edc7451c095a3e
ekaterina.naumovic <ekaterina.naumovic@aliexpress.ru>
2025-10-26 14:56:33 +0300

    Lint issues fixed in Ekaterina N: Homework 4

--- homework/ekaterina_naumovich/homework_4/task1.py
+++ homework/ekaterina_naumovich/homework_4/task1.py
@@ -1,8 +1,9 @@
 my_dict = {
-    'tuple': (1, 2, 3, 4, 5), 
+    'tuple': (1, 2, 3, 4, 5),
     'list': ['a', 'b', 'c', 'd', 'e'],
     'dict': {'cat': 'meow', 'dog': 'gav', 'cow': 'moo', 'duck': 'crya', 'sheep': 'bee'},
-    'set': {'Tom', 'Mike', 'Bob', 'Sam', 'Phil'} }
+    'set': {'Tom', 'Mike', 'Bob', 'Sam', 'Phil'} 
+    }
 
 print(my_dict['tuple'][-1])
 
