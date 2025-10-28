7b30cdca3b4359696e8d9aab568783a8e50ae794
ekaterina.naumovic <ekaterina.naumovic@aliexpress.ru>
2025-10-28 16:03:46 +0300

    Lint issues fixed in Homework 5

--- homework/ekaterina_naumovich/homework_5/task2.py
+++ homework/ekaterina_naumovich/homework_5/task2.py
@@ -3,13 +3,13 @@ result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 
 index1 = result1.index(':') + 2
-number1 = int(result1[index1:]) 
+number1 = int(result1[index1:])
 print(number1 + 10)
 
 index2 = result2.index(':') + 2
-number2 = int(result2[index2:]) 
+number2 = int(result2[index2:])
 print(number2 + 10)
 
 index3 = result3.index(':') + 2
-number3 = int(result3[index3:]) 
+number3 = int(result3[index3:])
 print(number3 + 10)
