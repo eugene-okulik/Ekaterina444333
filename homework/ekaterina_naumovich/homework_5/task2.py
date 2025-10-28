27ddd56b02852350eef0a063e19a53724e014c37
ekaterina.naumovic <ekaterina.naumovic@aliexpress.ru>
2025-10-28 16:00:45 +0300

    Ekaterina Naumovich Homework 5 complete

---
+++ homework/ekaterina_naumovich/homework_5/task2.py
@@ -0,0 +1,15 @@
+result1 = 'результат операции: 42'
+result2 = 'результат операции: 514'
+result3 = 'результат работы программы: 9'
+
+index1 = result1.index(':') + 2
+number1 = int(result1[index1:]) 
+print(number1 + 10)
+
+index2 = result2.index(':') + 2
+number2 = int(result2[index2:]) 
+print(number2 + 10)
+
+index3 = result3.index(':') + 2
+number3 = int(result3[index3:]) 
+print(number3 + 10)
---
+++ homework/ekaterina_naumovich/homework_5/task3.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+name1, name2, name3 = students
+subjects = ['math', 'biology', 'geography']
+subject1, subject2, subject3 = subjects
+
+text = f'Students {name1}, {name2}, {name3} study these subjects: {subject1}, {subject2}, {subject3}'
+print(text)
