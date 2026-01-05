c5217cc14987c0f533cfd39d0de6b3aaf68a3205
ekaterina.naumovic <ekaterina.naumovic@aliexpress.ru>
2026-01-05 14:47:01 +0300

    Lint issues fixed

--- homework/ekaterina_naumovich/homework_23/locustfile.py
+++ homework/ekaterina_naumovich/homework_23/locustfile.py
@@ -6,17 +6,17 @@ class Objects (HttpUser):
 
     @task(1)
     def get_all_objects(self):
-        response = self.client.get('')
+        self.client.get('')
 
     @task(3)
     def get_one_object(self):
-        id = {random.randint(25,30)}
-        response = self.client.get(f'/{id}')
-        assert(id == 29), 'Secret object'
+        id = {random.randint(25, 30)}
+        self.client.get(f'/{id}')
+        assert id == 29, 'Secret object'
 
     @task(2)
     def create_one_object(self):
-        response = self.client.post(
+        self.client.post(
             '',
             json={
                 'name': 'Sam',
